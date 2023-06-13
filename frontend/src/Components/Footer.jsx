6d8de9439b938bbdc4493734646983dbe3ea4bd0
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-06-13 15:39:43 +0530

    ok

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <Heading>Footer</Heading>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+const Navbar = () => {
+  return (
+    <div>
+      <Heading>Navbar</Heading>
+    </div>
+  )
+}
+
+export default Navbar

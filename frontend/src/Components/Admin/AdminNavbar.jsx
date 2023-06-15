1a2670ff486e3e30a1cad640b693375c0578d6c1
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-06-15 20:56:31 +0530

    fw24_600_Day3

--- frontend/src/Components/Admin/AdminNavbar.jsx
+++ frontend/src/Components/Admin/AdminNavbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 const AdminNavbar = () => {
-  const total = useSelector(state=> state.dataReducer.total)
-  console.log(total)
+  // const total = useSelector(state=> state.dataReducer.total)
+  // console.log(total)
   return (
     <div className='text-white w-full flex m-5 '>
       <h2 className='mx-5 px-9 py-3 rounded-xl bg-orange-600'> <Link to ={'/admin'}>Users {}</Link></h2>
       <h2 className='mx-5 px-9 py-3 rounded-xl bg-orange-600'>Total Cash :
        {/* {total} */}
        </h2>
+       <Sidebar/>
     </div>
   )
 }

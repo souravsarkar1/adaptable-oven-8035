61a97d84597fd8ab8b3ceac576a96bfaf36defc0
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-06-16 11:27:52 +0530

    update

--- frontend/src/Pages/Allroutes.jsx
+++ frontend/src/Pages/Allroutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './Home'
+import ProfilePage from './ProfilePages/ProfilePage'
 const Allroutes = () => {
   return (
     <div>
       <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<ProfilePage/>}/>
       </Routes>
     </div>
   )

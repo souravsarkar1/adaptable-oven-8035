515e86abb2ab135f3ecdc42bac6720c31f8a36af
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-06-16 15:02:38 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-require('dotenv').config();
 const { connection } = require('./db');
+const { userRoute } = require('./routes/user.routes');
 
+
+require('dotenv').config();
 const app = express();
+app.use(express.json());
+
+app.use('/users', userRoute);
 
 app.listen(process.env.port, async () => {
     try {
-
         await connection;
-        console.log("Connected To Data Base");
-        console.log(`Server Is Running At Port No. ${process.env.port}`);
-
+        console.log(`port is running at ${process.env.port}`);
+        console.log("connected to db");
     } catch (error) {
         console.log(error.message);
-        console.log("Something Went To Wrong");
+        console.log("Sometthing went to wrong");
     }
 })
